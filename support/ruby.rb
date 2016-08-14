29446a35dd0f66f77ceba3ae97b8ce1d44f801c1
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-08-14 21:44:43 +1200

    Allow class methods to be called in Ruby

--- support/ruby.rb
+++ support/ruby.rb
@@ -1,3 +1,9 @@
+module PluggerInternal
+  def self.normalize_method_names(names)
+    names.map { |method| ((/(.*)_internal/ =~ method.to_s) ? $1 : method).to_sym }
+  end
+end
+
 class PluggerObject
   attr_reader :object_pointer
 
@@ -5,7 +11,16 @@ class PluggerObject
     @object_pointer = object_pointer
   end
 
+  def methods
+    PluggerInternal.normalize_method_names(super)
+  end
+
+  def self.methods
+    PluggerInternal.normalize_method_names(super)
+  end
+
   def method_missing(name, *original_args)
+    return super unless methods.include?(name)
     method_pointer = self.class.pointer_to_function(name)
 
     if method_pointer
@@ -22,19 +37,27 @@ class PluggerObject
     end
   end
 
-  # TODO: do this for singleton_methods too
-  def methods
-    super.map do |method|
-      if /(.*)_internal/ =~ method.to_s
-        $1
-      else
-        method
-      end.to_sym
+  def self.method_missing(name, *original_args)
+    return super unless methods.include?(name)
+    method_pointer = self.pointer_to_function(name)
+
+    if method_pointer
+      # we have C functions plugged in, with the '_internal' suffix.
+      # here, we call the internal shim and give it the pointer
+      # to the Rust function.
+      internal_method = "#{name}_internal".to_sym
+
+      arguments = [method_pointer] + original_args
+
+      send(internal_method, *arguments)
+    else
+      super
     end
   end
 
   def self.pointer_to_function(name)
     pointer_const_name = name.to_s.upcase
+    puts "checking: #{pointer_const_name}"
     self.const_get(pointer_const_name)
   end
 end
